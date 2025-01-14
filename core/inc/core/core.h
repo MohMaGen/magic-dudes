4a08ef70fc5e9ffd0aa9142dbb1536a6443f9c66
MohMaGen <mark.mokhov@yandex.ru>
2025-01-14 13:16:19 +0300

    sync

--- core/inc/core/core.h
+++ core/inc/core/core.h
@@ -15,7 +15,10 @@ typedef struct {
 	} main_hero;
 } state_t;
 
-enum { MAIN_MENU, CREATE_CHARACTER, GAME, STATES_COUNT } state_e; 
+typedef enum {
+	MAIN_MENU = 0, CREATE_CHARACTER, GAME,
+	STATES_COUNT
+} state_e; 
 
 state_config_t states[STATES_COUNT] = {
 	[MAIN_MENU] = (state_config_t) {
@@ -34,8 +37,12 @@ state_config_t states[STATES_COUNT] = {
 
 int main(void)
 {
-	core_t core = { 0 };
-	core = core_init();
+	state_t state = {
+		.main_hero.hp  = 10,
+		.main_hero.pos = (vector2_t) { 10.0f, 20.0f },
+	};
+
+	core_ptr_t = core_init(state, MAIN_MENU);
 
 	core_init_state(states, STATES_COUNT);
 
@@ -55,7 +62,11 @@ int main(void)
 */
 
 typedef struct {
+	void  *data;
+	long   state; 
+	size_t size;
 } core_t;
+
 
 
 #endif
